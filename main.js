68b0a5ee0343322a11c0a74c1a7fa1596c4805c0
A7madtabaza <ahmadtabaza272@gmail.com>
2024-12-27 15:15:45 +0300

    js task

---
+++ main.js
@@ -0,0 +1,40 @@
+console.log(-5*3);
+console.log('javascript'+50);
+console.log(17 %  5);
+console.log(5%17);
+console.log(5/10);
+console.log(4==='4');
+console.log(4!=5);
+console.log(7<=8);
+console.log ('hello'+5);
+let x=7.1
+let y=3.2
+console.log(Math.ceil(x)-Math.floor(y));
+let s=4
+console.log(Math.pow(s,2))
+
+
+
+let a = prompt("Please enter a number:");
+alert("You entered: " +a);
+
+let c = parseInt(prompt("Please enter a number:"));
+let d = parseInt(prompt("Please enter a number:"));
+ let e=c+d;
+console.log(e)
+
+
+let q = parseInt(prompt("Please enter a number:"));
+let w = parseInt(prompt("Please enter a number:"));
+let j=q*w;
+console.log(j)
+
+let l = parseInt(prompt("Please enter a number:"));
+let p = parseInt(prompt("Please enter a number:"));
+let m = l / p;
+console.log(m)
+
+let t = parseFloat(prompt("Please enter a number:"));
+let n = parseFloat(prompt("Please enter a number:"));
+let v=t+n;
+console.log(v);
